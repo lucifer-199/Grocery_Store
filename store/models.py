f400abf9ad2d54eb7462dc7fbebbd1885e63e56c
Aniket Pratyush <51285661+lucifer-199@users.noreply.github.com>
2021-03-04 12:26:08 +0530

    Second Commit going to host it

--- store/models.py
+++ store/models.py
@@ -8,16 +8,19 @@ class Product(models.Model):
     item_status= models.CharField(max_length=300)
     pub_date = models.DateField()
     is_saved= models.BooleanField(default=False)
+    user_id= models.CharField(max_length=1000)
 
     def __str__(self):
         return self.item_name
     
-class SavedItems(models.Model):
+class SavedItem(models.Model):
     item_id = models.AutoField(primary_key=True)
     item_name = models.CharField(max_length=100)
     item_quantity= models.CharField(max_length=100)
     item_status= models.CharField(max_length=300)
     pub_date = models.DateField()
+    user_id= models.CharField(max_length=1000)
+    prev_prod_id=models.CharField(max_length=1000,default="")
 
     def __str__(self):
         return self.item_name
--- store/views.py
+++ store/views.py
@@ -1,94 +1,140 @@
 
 from django.shortcuts import render, HttpResponse, redirect
-from .models import Product, SavedItems
+from .models import Product, SavedItem
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate,login,logout
 
 def home(request):
-    items= Product.objects.all()
+    items= Product.objects.filter(user_id=request.user.id).order_by('-pub_date')
     context={'items':items}
     return render(request,'store/home.html',context)
 
 def add(request):
-    if request.method == 'POST':
-        name= request.POST['name']        
-        quantity= request.POST['qty']        
-        status= request.POST['status']        
-        date= request.POST['date']  
-
-        if len(name)<2 :
-            messages.error(request,"Please fill the form correctly")
-        else :
-            messages.success(request," Item has been added successfully ")
-        product = Product(item_name=name,item_quantity=quantity,item_status=status,pub_date=date)
-        product.save()
-    return render(request,'store/add.html')
+    if request.user.id != None :
+        if request.method == 'POST':
+            name= request.POST['name']        
+            quantity= request.POST['qty']        
+            status= request.POST['status']        
+            date= request.POST['date'] 
+            if len(name)<2 :
+                messages.error(request,"Please fill the form correctly")
+            else :
+                messages.success(request," Item has been added successfully ")
+            product = Product(item_name=name,item_quantity=quantity,item_status=status,pub_date=date,user_id=request.user.id)
+            product.save()
+        return render(request,'store/add.html')
+    else :
+        messages.error(request,"Please Log In to add items")
+        return redirect('home')
+        
 
 def updateView(request,myid):
-    item= Product.objects.filter(item_id=myid).first()
-    context={'item':item}
-    return render(request,'store/updateView.html',context)
+    if request.user.id != None :
+        item= Product.objects.filter(item_id=myid).first()
+        context={'item':item}
+        return render(request,'store/updateView.html',context)
+    else :
+        messages.error(request,"Please Log In to Update items")
+        return redirect('home')
+    
 
 def update(request,myid):
-    if request.method == 'POST':
-        item= Product.objects.filter(item_id=myid).first()
-        name= request.POST['name']        
-        quantity= request.POST['qty']        
-        status= request.POST['status']        
-        date= request.POST['date'] 
-        item.item_name= name
-        item.item_quantity=quantity
-        item.item_status= status
-        item.pub_date=date
-        item.save()
+    if request.user.id != None :
+        if request.method == 'POST':
+            item= Product.objects.filter(item_id=myid).first()
+            name= request.POST['name']        
+            quantity= request.POST['qty']        
+            status= request.POST['status']        
+            date= request.POST['date'] 
+            item.item_name= name
+            item.item_quantity=quantity
+            item.item_status= status
+            item.pub_date=date
+            item.is_saved=False
+            item.save()
+            return redirect('home')
+    else:
+        messages.error(request,"Please Log In to Update items")
         return redirect('home')
+        
     
 def deleted(request,myid):
-    item=Product.objects.filter(item_id=myid).first()
-    item.delete()
-    return redirect('home')
+    if request.user.id != None :
+        item=Product.objects.filter(item_id=myid).first()
+        item.delete()
+        return redirect('home')
+    else:
+        messages.error(request,"Please Log In to Delete items")
+        return redirect('home')
+        
 
 def query(request):
-    if request.method== 'POST':
-        rdate= request.POST['query']
-        items=Product.objects.filter(pub_date=rdate)
-        if items.count()==0:
-            messages.error(request,"No search results found. Please refine your query")
-            return render(request,'store/query.html')
-        else :
-            context={'items':items}
-            messages.success(request,"Your Search Results are : ")
-            return render(request,'store/query.html',context)
+    if request.user.id != None :
+        if request.method== 'POST':
+            rdate= request.POST['query']
+            items1=Product.objects.filter(pub_date=rdate)
+            items2=Product.objects.filter(user_id=request.user.id)
+            items=items1.intersection(items2)
+            if items.count()==0:
+                messages.error(request,"No search results found. Please refine your query")
+                context={'rdate':rdate}
+                return render(request,'store/query.html',context)
+            else :
+                context={'items':items,'rdate':rdate}
+                return render(request,'store/query.html',context)
+    else:
+        messages.error(request,"You are not a Valid User. Please Log In to Filter List items.")
+        return redirect('home')
+        
 
 def saveItem(request,myid):
-    item= Product.objects.filter(item_id=myid).first()
-    flag= item.is_saved
-    if flag== True:
-        messages.error(request,"This item is already added in Saved Items List. Try to add something New !")
-        return redirect('home')
+    if request.user.id != None :
+        item= Product.objects.filter(item_id=myid).first()
+        flag= item.is_saved
+        if flag== True:
+            messages.error(request,"This item is already added in Saved Items List. Try to add something New !")
+            return redirect('home')
+        else:
+            item.is_saved= True
+            item.save()
+            sitem= SavedItem(item_name=item.item_name,item_quantity=item.item_quantity,item_status=item.item_status,pub_date=item.pub_date,user_id=request.user.id,prev_prod_id=myid)
+            sitem.save()
+            messages.success(request,"Selected item has been successfully added to your Saved Items List!")
+            return redirect('home')
     else:
-        item.is_saved= True
-        item.save()
-        product= SavedItems(item_name=item.item_name,item_quantity=item.item_quantity,item_status=item.item_status,pub_date=item.pub_date)
-        product.save()
-        messages.success(request,"Selected item has been successfully added to your Saved Items List!")
+        messages.error(request,"Please Log In to save items to Saved Items List")
         return redirect('home')
+        
     
 def displaySavedItems(request):
-    sitems= SavedItems.objects.all()
-    
-    if sitems.count()==0:
-        messages.error(request,"Your Saved Items List is Empty !")
-        return render(request,'store/savedList.html')
+    if request.user.id != None :
+        sitems= SavedItem.objects.filter(user_id=request.user.id)
+        
+        if sitems.count()==0:
+            messages.error(request,"Your Saved Items List is Empty !")
+            return render(request,'store/savedList.html')
+        else:
+            context= {'sitems':sitems}
+            return render(request,'store/savedList.html',context)
     else:
-        context= {'sitems':sitems}
-        return render(request,'store/savedList.html',context)
+        messages.error(request,"Please Log In to display items of Saved Items List")
+        return redirect('home')
+        
 
 def deleteSavedItems(request,myid):
-    sitem=SavedItems.objects.filter(item_id=myid).first()
-    sitem.delete()
-    return redirect('displaySavedItems')
+    if request.user.id != None :
+        sitem1=SavedItem.objects.filter(item_id=myid).first()
+        item1= Product.objects.filter(item_id=sitem1.prev_prod_id).first()
+        sitem1.delete()
+        if item1!= None:
+            item1.is_saved=False
+            item1.save()
+        return redirect('displaySavedItems')
+    else:
+        messages.error(request,"You must be Logged In to delete saved items.")
+        return redirect('home')
+        
     
       
         
